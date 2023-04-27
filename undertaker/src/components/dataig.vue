24ac314e4763e4a642ec9d9fec49ac0d840abdec
R1chi333 <angelaguo006@gmail.com>
2023-04-27 13:23:34 -0400

    .

---
+++ undertaker/src/components/dataig.vue
@@ -0,0 +1,24 @@
+<script>
+import container from './container.vue'
+
+export default {
+  components: {
+    container
+  },
+  data() {
+    return {
+      play: true
+    }
+  },
+  methods: {
+    togglePlay() {
+      this.play = !this.play
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  /* overflow-y: hidden; */
+}
+</style>
