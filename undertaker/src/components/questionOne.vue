f1391887a8bc60381f7d8e2bfd5f3987f7ee145c
richie <113203265+R1chi333@users.noreply.github.com>
2023-04-27 11:11:50 -0400

    .

--- undertaker/src/components/questionOne.vue
+++ undertaker/src/components/questionOne.vue
@@ -1,22 +1,48 @@
 <template>
-  <transition>
+  <transition mode="out-in">
     <div v-if="play" id="questionOne">
       <h3>CHOICE ONE</h3>
-      <p>CAUSE:</p>
-      <p>YEAR:</p>
-      <p>SEX:</p>
-      <p>RACE/ETHNICITY:</p>
+      <p>CAUSE: {{ cause }}</p>
+      <p>YEAR: {{ year }}</p>
+      <p>SEX: {{ sex }}</p>
+      <p>RACE/ETHNICITY: {{ race }}</p>
       <button @click="$emit('toggle-play')">BUTTON</button>
+      <button @click="fetchRandomRecord">Fetch random record</button>
     </div>
   </transition>
 </template>
 
 <script>
+
 export default {
   props: {
     play: Boolean
+  },
+  data() {
+    return {
+      deaths: [],
+      cause: '',
+      year: '',
+      sex: '',
+      race: ''
+    };
+  },
+  async created() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json');
+    const data = await response.json();
+    this.deaths = data;
+  },
+  methods: {
+    fetchRandomRecord() {
+      const randomIndex = Math.floor(Math.random() * this.deaths.length);
+      this.cause = this.deaths[randomIndex].leading_cause;
+      this.year = this.deaths[randomIndex].year;
+      this.sex = this.deaths[randomIndex].sex;
+      this.race = this.deaths[randomIndex].race_ethnicity;
+    }
   }
-}
+};
+
 </script>
 
 <style scoped>
